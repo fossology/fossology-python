c77fa8900ab82406a36243531dd36819eb31783c
Marion Deveaud <marion.deveaud@siemens.com>
2020-07-19 22:54:03 +0200

    refactor(tests): use pytest instead of unittest

--- tests/test_folders.py
+++ tests/test_folders.py
@@ -2,111 +2,91 @@
 # SPDX-License-Identifier: MIT
 
 import time
+import pytest
 import secrets
-import unittest
 
-from test_base import foss, logger
+from fossology import Fossology
 from fossology.obj import Folder
-from fossology.exceptions import FossologyApiError, AuthorizationError
+from fossology.exceptions import AuthorizationError, FossologyApiError
 
 
-class TestFossologyFolders(unittest.TestCase):
-    def test_create_folder(self):
-        name = "FossPythonTest"
-        desc = "Created via the Fossology Python API"
-        # Create folder for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.create_folder(foss.rootFolder, name, description=desc, group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
+def test_create_folder(foss: Fossology):
+    name = "FossPythonTest"
+    desc = "Created via the Fossology Python API"
+    # Create folder for unknown group
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.create_folder(foss.rootFolder, name, description=desc, group="test")
+    assert "Folder creation for group test in folder 1 not authorized" in str(
+        excinfo.value
+    )
 
-        test_folder = foss.create_folder(foss.rootFolder, name, description=desc)
-        self.assertEqual(
-            test_folder.name, name, f"Main test {name} folder couldn't be created"
-        )
-        self.assertEqual(
-            test_folder.description,
-            desc,
-            "Description of folder on the server is wrong",
-        )
+    test_folder = foss.create_folder(foss.rootFolder, name, description=desc)
+    assert test_folder.name == name
+    assert test_folder.description == desc
 
-        # Recreate folder to test API response 200
-        test_folder = foss.create_folder(foss.rootFolder, name, description=desc)
-        self.assertEqual(
-            test_folder.name, name, f"Main test {name} folder couldn't be created"
-        )
+    # Recreate folder to test API response 200
+    test_folder = foss.create_folder(foss.rootFolder, name, description=desc)
+    assert test_folder.name == name
 
-        # Create folder in arbitrary parent
-        parent = Folder(secrets.randbelow(1000), "Parent", "", 0)
-        self.assertRaises(AuthorizationError, foss.create_folder, parent, "No Parent")
-
-        foss.delete_folder(test_folder)
-
-    def test_update_folder(self):
-        name = "FossPythonFolderUpdate"
-        desc = "Created via the Fossology Python API"
-        update_folder = foss.create_folder(foss.rootFolder, name, desc)
-
-        name = "NewFolderName"
-        desc = "Updated via the Fossology Python API"
-        update_folder = foss.update_folder(update_folder, name=name, description=desc)
-        self.assertEqual(update_folder.name, name, "Folder name couldn't be updated")
-        self.assertEqual(
-            update_folder.description, desc, "Folder description couldn't be updated"
-        )
-        foss.delete_folder(update_folder)
-
-    def test_move_folder(self):
-        move_copy_folder = foss.create_folder(
-            foss.rootFolder, "MoveCopyTest", "Test move() and copy() functions"
-        )
-        test_folder = foss.create_folder(
-            foss.rootFolder, "TestFolder", "Folder to be moved and copied via API"
-        )
-        try:
-            test_folder = foss.move_folder(test_folder, move_copy_folder)
-            self.assertEqual(
-                test_folder.parent,
-                move_copy_folder.id,
-                "Folder was not moved to the expected location",
-            )
-        except FossologyApiError as error:
-            logger.error(error.message)
-
-        try:
-            folder_copy = foss.copy_folder(test_folder, foss.rootFolder)
-            folder_list = foss.list_folders()
-            folder_copy = [
-                folder
-                for folder in folder_list
-                if folder.parent == foss.rootFolder.id and folder.name == "TestFolder"
-            ]
-            self.assertIsNotNone(
-                folder_copy, "Folder was not copied to the expected location"
-            )
-        except FossologyApiError as error:
-            logger.error(error.message)
-            return
-
-        foss.delete_folder(move_copy_folder)
-        foss.delete_folder(folder_copy[0])
-
-    def test_delete_folder(self):
-        folder = foss.create_folder(
-            foss.rootFolder, "ToBeDeleted", "Test folder deletion via API"
-        )
-        foss.delete_folder(folder)
-        time.sleep(3)
-        try:
-            deleted_folder = foss.detail_folder(folder)
-            self.assertIsNotNone(deleted_folder, "Deleted folder still exists")
-        except FossologyApiError as error:
-            logger.error(error.message)
+    # Cleanup
+    foss.delete_folder(test_folder)
 
 
-if __name__ == "__main__":
-    unittest.main()
-    foss.close()
+def test_create_folder_no_parent(foss: Fossology):
+    # Create folder in arbitrary parent
+    parent = Folder(secrets.randbelow(1000), "Parent", "", 0)
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.create_folder(parent, "FossFolderNoParent")
+    assert f"Folder creation in folder {parent.id} not authorized" in str(excinfo.value)
+
+
+def test_update_folder(foss: Fossology):
+    name = "FossPythonFolderUpdate"
+    desc = "Created via the Fossology Python API"
+    update_folder = foss.create_folder(foss.rootFolder, name, desc)
+
+    name = "NewFolderName"
+    desc = "Updated via the Fossology Python API"
+    update_folder = foss.update_folder(update_folder, name=name, description=desc)
+    assert update_folder.name == name
+    assert update_folder.description == desc
+
+    # Cleanup
+    foss.delete_folder(update_folder)
+
+
+def test_move_folder(foss: Fossology):
+    move_copy_folder = foss.create_folder(
+        foss.rootFolder, "MoveCopyTest", "Test move() and copy() functions"
+    )
+    test_folder = foss.create_folder(
+        foss.rootFolder, "TestFolder", "Folder to be moved and copied via API"
+    )
+    test_folder = foss.move_folder(test_folder, move_copy_folder)
+    assert test_folder.parent == move_copy_folder.id
+
+    folder_copy = foss.copy_folder(test_folder, foss.rootFolder)
+    folder_list = foss.list_folders()
+    folder_copy = [
+        folder
+        for folder in folder_list
+        if folder.parent == foss.rootFolder.id and folder.name == "TestFolder"
+    ]
+    assert folder_copy
+
+    # Cleanup
+    foss.delete_folder(move_copy_folder)
+    foss.delete_folder(folder_copy[0])
+
+
+def test_delete_folder(foss: Fossology):
+    folder = foss.create_folder(
+        foss.rootFolder, "ToBeDeleted", "Test folder deletion via API"
+    )
+    foss.delete_folder(folder)
+    time.sleep(3)
+    with pytest.raises(FossologyApiError) as excinfo:
+        foss.detail_folder(folder)
+    assert f"Error while getting details for folder ToBeDeleted ({folder.id})" in str(
+        excinfo.value
+    )
--- tests/test_uploads.py
+++ tests/test_uploads.py
@@ -1,324 +1,225 @@
 # Copyright 2019-2020 Siemens AG
 # SPDX-License-Identifier: MIT
 
-import time
 import secrets
-import unittest
+import pytest
 
-from test_base import foss, logger, test_files
+from fossology import Fossology
 from fossology.obj import AccessLevel, Folder, Upload, SearchTypes
 from fossology.exceptions import AuthorizationError, FossologyApiError
 
-upload_filename = "base-files_11.tar.xz"
+
+def test_upload_sha1(upload: Upload):
+    assert upload.uploadname == "base-files_11.tar.xz"
+    assert upload.filesha1 == "D4D663FC2877084362FB2297337BE05684869B00"
 
 
-def get_upload():
-    upload_list = foss.list_uploads()
-    test_upload = None
-    for upload in upload_list:
-        if upload.uploadname == upload_filename:
-            test_upload = upload
-            break
-    return test_upload
+def test_upload_nogroup(foss: Fossology, upload_folder: Folder, test_file_path: str):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.upload_file(
+            upload_folder,
+            file=test_file_path,
+            description="Test upload from github repository via python lib",
+            group="test",
+        )
+    assert (
+        f"Upload of {test_file_path} for group test in folder {upload_folder.id} not authorized"
+        in str(excinfo.value)
+    )
 
 
-def do_upload():
-    file_path = f"{test_files}/{upload_filename}"
-    test_upload = foss.upload_file(
+def test_get_uploads_nogroup(foss: Fossology):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.list_uploads(group="test")
+    assert "Retrieving list of uploads for group test not authorized" in str(
+        excinfo.value
+    )
+
+
+def test_get_uploads(foss: Fossology, upload_folder: Folder, test_file_path: str):
+    name = "FossPythonTestUploadsSubfolder"
+    desc = "Created via the Fossology Python API"
+    upload_subfolder = foss.create_folder(upload_folder, name, description=desc)
+    foss.upload_file(
+        upload_folder,
+        file=test_file_path,
+        description="Test upload from github repository via python lib",
+    )
+    foss.upload_file(
+        upload_subfolder,
+        file=test_file_path,
+        description="Test upload from github repository via python lib",
+    )
+    assert len(foss.list_uploads(folder=upload_folder)) == 2
+    assert len(foss.list_uploads(folder=upload_folder, recursive=False)) == 1
+    assert len(foss.list_uploads(folder=upload_subfolder)) == 1
+
+
+def test_upload_from_vcs(foss: Fossology):
+    vcs = {
+        "vcsType": "git",
+        "vcsUrl": "https://github.com/fossology/fossology-python",
+        "vcsName": "fossology-python-github-master",
+        "vcsUsername": "",
+        "vcsPassword": "",
+    }
+    vcs_upload = foss.upload_file(
         foss.rootFolder,
-        file=file_path,
-        description="Test upload via fossology-python lib",
+        vcs=vcs,
+        description="Test upload from github repository via python lib",
         access_level=AccessLevel.PUBLIC,
     )
-    time.sleep(3)
-    return test_upload
+    assert vcs_upload.uploadname == vcs["vcsName"]
+    assert not foss.search(searchType=SearchTypes.DIRECTORIES, filename=".git",)
+
+    # Cleanup
+    foss.delete_upload(vcs_upload)
 
 
-class TestFossologyUploads(unittest.TestCase):
-    def test_upload_file(self):
-        test_upload = get_upload()
-        if not test_upload:
-            test_upload = do_upload()
+def test_upload_ignore_scm(foss: Fossology):
+    vcs = {
+        "vcsType": "git",
+        "vcsUrl": "https://github.com/fossology/fossology-python",
+        "vcsName": "fossology-python-github-master",
+        "vcsUsername": "",
+        "vcsPassword": "",
+    }
+    vcs_upload = foss.upload_file(
+        foss.rootFolder,
+        vcs=vcs,
+        description="Test upload with ignore_scm flag",
+        ignore_scm=False,
+        access_level=AccessLevel.PUBLIC,
+    )
+    assert vcs_upload.uploadname == vcs["vcsName"]
+    # FIXME: shall be fixed in the next release
+    # assert foss.search(
+    #    searchType=SearchTypes.DIRECTORIES, filename=".git",
+    # ) == $something
 
-        self.assertEqual(
-            test_upload.uploadname, upload_filename, "Uploadname on the server is wrong"
-        )
-        self.assertEqual(
-            test_upload.filesha1,
-            "D4D663FC2877084362FB2297337BE05684869B00",
-            "Filesha1 on the server is wrong",
-        )
-
-    def test_get_uploads(self):
-        name = "FossPythonTestUploads"
-        desc = "Created via the Fossology Python API"
-        test_folder = foss.create_folder(foss.rootFolder, name, description=desc)
-        name = "FossPythonTestUploadsSubfolder"
-        test_subfolder = foss.create_folder(test_folder, name, description=desc)
-        test_file = f"{test_files}/{upload_filename}"
-
-        # Upload file for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.upload_file(
-                test_folder,
-                file=test_file,
-                description="Test upload from github repository via python lib",
-                group="test",
-            )
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-        foss.upload_file(
-            test_folder,
-            file=test_file,
-            description="Test upload from github repository via python lib",
-        )
-        foss.upload_file(
-            test_subfolder,
-            file=test_file,
-            description="Test upload from github repository via python lib",
-        )
-
-        # Get uploads from unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.list_uploads(group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-        folder_uploads = foss.list_uploads(folder=test_folder)
-        no_subfolder_uploads = foss.list_uploads(folder=test_folder, recursive=False)
-        self.assertEqual(len(folder_uploads), 2, "Too many uploads listed")
-        self.assertEqual(len(no_subfolder_uploads), 1, "Too many uploads listed")
-
-        # Cleanup
-        foss.delete_folder(test_folder)
-
-    def test_upload_from_vcs(self):
-        vcs = {
-            "vcsType": "git",
-            "vcsUrl": "https://github.com/fossology/fossology-python",
-            "vcsName": "fossology-python-github-master",
-            "vcsUsername": "",
-            "vcsPassword": "",
-        }
-        vcs_upload = foss.upload_file(
-            foss.rootFolder,
-            vcs=vcs,
-            description="Test upload from github repository via python lib",
-            access_level=AccessLevel.PUBLIC,
-        )
-        self.assertEqual(
-            vcs_upload.uploadname, vcs["vcsName"], "Uploadname on the server is wrong",
-        )
-        search_result = foss.search(
-            searchType=SearchTypes.DIRECTORIES, filename=".git",
-        )
-        self.assertEqual(search_result, [], "Search result should be empty")
-        foss.delete_upload(vcs_upload)
-
-    def test_upload_ignore_scm(self):
-        vcs = {
-            "vcsType": "git",
-            "vcsUrl": "https://github.com/fossology/fossology-python",
-            "vcsName": "fossology-python-github-master",
-            "vcsUsername": "",
-            "vcsPassword": "",
-        }
-        vcs_upload = foss.upload_file(
-            foss.rootFolder,
-            vcs=vcs,
-            description="Test upload with ignore_scm flag",
-            ignore_scm=False,
-            access_level=AccessLevel.PUBLIC,
-        )
-        self.assertEqual(
-            vcs_upload.uploadname, vcs["vcsName"], "Uploadname on the server is wrong",
-        )
-        search_result = foss.search(
-            searchType=SearchTypes.DIRECTORIES, filename=".git",
-        )
-        self.assertEqual(search_result, [], "A .git directory has been found by search")
-
-        # FIXME: shall be fixed in the next release
-        # self.assertEqual(search_result, $something, "Search result should be empty")
-
-        foss.delete_upload(vcs_upload)
-
-    def test_upload_from_url(self):
-        url = {
-            "url": "https://github.com/fossology/fossology-python/archive/master.zip",
-            "name": "fossology-python-master.zip",
-            "accept": "zip",
-            "reject": "",
-            "maxRecursionDepth": "1",
-        }
-        url_upload = foss.upload_file(
-            foss.rootFolder,
-            url=url,
-            description="Test upload from url via python lib",
-            access_level=AccessLevel.PUBLIC,
-        )
-        self.assertEqual(
-            url_upload.uploadname, url["name"], "Uploadname on the server is wrong",
-        )
-        foss.delete_upload(url_upload)
-
-    def test_empty_upload(self):
-        empty_upload = foss.upload_file(
-            foss.rootFolder,
-            description="Test empty upload",
-            access_level=AccessLevel.PUBLIC,
-        )
-        self.assertIsNone(
-            empty_upload, "Something has been uploaded for empty upload test"
-        )
-
-    def test_move_upload(self):
-        test_upload = get_upload()
-        if not test_upload:
-            test_upload = do_upload()
-            logger.debug(f"{test_upload.id} uploaded from test 'test_move_upload'")
-
-        move_folder = foss.create_folder(
-            foss.rootFolder, "MoveUploadTest", "Test move upload function"
-        )
-        # Move upload for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.move_upload(test_upload, move_folder, group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-        foss.move_upload(test_upload, move_folder)
-        moved_upload = foss.detail_upload(test_upload.id)
-        self.assertEqual(
-            moved_upload.folderid, move_folder.id, "Upload couldn't be moved"
-        )
-
-        # Move/Copy to arbitrary folder
-        non_folder = Folder(secrets.randbelow(1000), "Non folder", "", foss.rootFolder)
-        self.assertRaises(AuthorizationError, foss.move_upload, test_upload, non_folder)
-        self.assertRaises(AuthorizationError, foss.copy_upload, test_upload, non_folder)
-
-        # FIXME: recursion due to https://github.com/fossology/fossology/pull/1748?
-        foss.copy_upload(moved_upload, foss.rootFolder)
-        list_uploads = foss.list_uploads()
-        test_upload = None
-        for upload in list_uploads:
-            if upload.folderid == foss.rootFolder.id:
-                test_upload = upload
-        if not test_upload:
-            logger.error("Coyping uploads didn't work")
-        else:
-            logger.info("Copying uploads works again, replace log output with assert")
-        # self.assertIsNotNone(test_upload, "Upload couldn't be copied")
-
-        # Clean up
-        foss.delete_folder(move_folder)
-
-    def test_upload_summary(self):
-        test_upload = get_upload()
-        if not test_upload:
-            test_upload = do_upload()
-
-        summary = foss.upload_summary(test_upload)
-        self.assertEqual(summary.clearingStatus, "Open")
-        self.assertEqual(summary.mainLicense, None)
-
-        # Get upload summary for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.upload_summary(test_upload, group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-    def test_upload_licenses(self):
-        test_upload = get_upload()
-        if not test_upload:
-            test_upload = do_upload()
-
-        licenses = foss.upload_licenses(test_upload)
-        self.assertEqual(
-            len(licenses),
-            56,
-            "Unexpected licenses were found for upload {test_upload.uploadname}",
-        )
-
-        # Get licenses with containers
-        licenses = foss.upload_licenses(test_upload, containers=True)
-        self.assertEqual(
-            len(licenses),
-            56,
-            "Unexpected licenses were found for upload {test_upload.uploadname}",
-        )
-
-        # Get licenses from unscheduled agent 'ojo'
-        licenses = foss.upload_licenses(test_upload, agent="ojo")
-        self.assertIsNone(
-            licenses[0].get("agentFindings"),
-            "Unexpected ojo licenses were found for upload {test_upload.uploadname}",
-        )
-
-        # Get licenses from another agent 'monk'
-        licenses = foss.upload_licenses(test_upload, agent="monk")
-        self.assertEqual(
-            len(licenses),
-            23,
-            "Unexpected licenses were found for upload {test_upload.uploadname}",
-        )
-
-        # Get license from upload for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.upload_licenses(test_upload, group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-    def test_delete_unknown_upload(self):
-        non_upload = Upload(
-            foss.rootFolder,
-            "Root Folder",
-            secrets.randbelow(1000),
-            "",
-            "Non Upload",
-            "2020-05-05",
-            "0",
-            "sha",
-        )
-        self.assertRaises(FossologyApiError, foss.delete_upload, non_upload)
-
-    def test_delete_upload(self):
-        test_upload = get_upload()
-        if not test_upload:
-            test_upload = do_upload()
-
-        # Delete upload for unknown group
-        with self.assertRaises(AuthorizationError) as cm:
-            foss.delete_upload(test_upload, group="test")
-        self.assertIn(
-            "Provided group:test does not exist (403)",
-            cm.exception.message,
-            "Exception message does not match requested group",
-        )
-
-        foss.delete_upload(test_upload)
-
-        verify_uploads = foss.list_uploads()
-        self.assertEqual(len(verify_uploads), 0, "Upload couldn't be deleted")
+    # Cleanup
+    foss.delete_upload(vcs_upload)
 
 
-if __name__ == "__main__":
-    unittest.main()
-    foss.close()
+def test_upload_from_url(foss: Fossology):
+    url = {
+        "url": "https://github.com/fossology/fossology-python/archive/master.zip",
+        "name": "fossology-python-master.zip",
+        "accept": "zip",
+        "reject": "",
+        "maxRecursionDepth": "1",
+    }
+    url_upload = foss.upload_file(
+        foss.rootFolder,
+        url=url,
+        description="Test upload from url via python lib",
+        access_level=AccessLevel.PUBLIC,
+    )
+    assert url_upload.uploadname == url["name"]
+
+    # Cleanup
+    foss.delete_upload(url_upload)
+
+
+def test_empty_upload(foss: Fossology):
+    empty_upload = foss.upload_file(
+        foss.rootFolder,
+        description="Test empty upload",
+        access_level=AccessLevel.PUBLIC,
+    )
+    assert not empty_upload
+
+
+def test_move_upload_nogroup(foss: Fossology, upload: Upload, move_folder: Folder):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.move_upload(upload, move_folder, group="test")
+    assert (
+        f"Moving upload {upload.id} for group test in folder {move_folder.id} not authorized"
+        in str(excinfo.value)
+    )
+
+
+def test_move_copy_upload(foss: Fossology, upload: Upload, move_folder: Folder):
+    foss.move_upload(upload, move_folder)
+    moved_upload = foss.detail_upload(upload.id)
+    assert moved_upload.folderid == move_folder.id
+
+    # FIXME: recursion due to https://github.com/fossology/fossology/pull/1748?
+    foss.copy_upload(moved_upload, foss.rootFolder)
+    list_uploads = foss.list_uploads()
+    test_upload = None
+    for upload in list_uploads:
+        if upload.folderid == foss.rootFolder.id:
+            test_upload = upload
+    if not test_upload:
+        print("Copying uploads didn't work")
+    else:
+        print("Copying uploads works again, replace log output with assert")
+    # assert upload
+
+
+def test_move_copy_arbitrary_folder(foss: Fossology, upload: Upload):
+    non_folder = Folder(secrets.randbelow(1000), "Non folder", "", foss.rootFolder)
+    with pytest.raises(AuthorizationError):
+        foss.move_upload(upload, non_folder)
+    with pytest.raises(AuthorizationError):
+        foss.copy_upload(upload, non_folder)
+
+
+def test_upload_summary(foss: Fossology, scanned_upload: Upload):
+    summary = foss.upload_summary(scanned_upload)
+    assert summary.clearingStatus == "Open"
+    assert not summary.mainLicense
+
+
+def test_upload_summary_nogroup(foss: Fossology, upload: Upload):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.upload_summary(upload, group="test")
+    assert f"Getting summary of upload {upload.id} for group test not authorized" in str(excinfo.value)
+
+
+def test_upload_licenses(foss: Fossology, scanned_upload: Upload):
+    licenses = foss.upload_licenses(scanned_upload)
+    assert len(licenses) == 56
+
+
+def test_upload_licenses_containers(foss: Fossology, scanned_upload: Upload):
+    licenses = foss.upload_licenses(scanned_upload, containers=True)
+    assert len(licenses) == 56
+
+
+def test_upload_licenses_unscheduled(foss: Fossology, scanned_upload: Upload):
+    licenses = foss.upload_licenses(scanned_upload, agent="ojo")
+    assert not licenses[0].get("agentFindings")
+
+
+def test_upload_licenses_from_agent(foss: Fossology, scanned_upload: Upload):
+    licenses = foss.upload_licenses(scanned_upload, agent="monk")
+    assert len(licenses) == 23
+
+
+def test_upload_licenses_nogroup(foss: Fossology, upload: Upload):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.upload_licenses(upload, group="test")
+    assert f"Getting license for upload {upload.id} for group test not authorized" in str(excinfo.value)
+
+
+def test_delete_unknown_upload(foss: Fossology):
+    upload = Upload(
+        foss.rootFolder,
+        "Root Folder",
+        secrets.randbelow(1000),
+        "",
+        "Non Upload",
+        "2020-05-05",
+        "0",
+        "sha",
+    )
+    with pytest.raises(FossologyApiError):
+        foss.delete_upload(upload)
+
+
+def test_delete_upload_nogroup(foss: Fossology, upload: Upload):
+    with pytest.raises(AuthorizationError) as excinfo:
+        foss.delete_upload(upload, group="test")
+    assert f"Deleting upload {upload.id} for group test not authorized" in str(excinfo.value)
