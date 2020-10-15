04ca22d7de6b70654b85315ce408fb71acc63a8f
Marion Deveaud <marion.deveaud@siemens.com>
2020-10-15 15:06:46 +0200

    refactor(0.2.0): get report name from HTTP header
    Interface change for `download_report`:
    - only the report id is accepted as parameter
    - removed `as_zip` option, Fossology only returns uncompressed
    reports
    - return a tuple of the report content and the file name as
    returned by the HTTP GET reponse header
    - use typehints and fix exceptions descriptions

--- tests/test_report.py
+++ tests/test_report.py
@@ -7,7 +7,7 @@ import mimetypes
 
 from pathlib import Path
 from test_base import foss, logger
-from test_uploads import get_upload, do_upload, upload_filename
+from test_uploads import get_upload, do_upload
 from fossology.exceptions import FossologyApiError
 from fossology.obj import ReportFormat
 
@@ -28,39 +28,20 @@ class TestFossologyReport(unittest.TestCase):
 
         try:
             # Plain text
-            report = foss.download_report(report_id)
-            report_path = Path.cwd() / "tests/files"
-            report_name = upload_filename + ".spdx-report.rdf"
-            with open(report_path / report_name, "w+") as report_file:
+            report, name = foss.download_report(report_id)
+            report_path = Path.cwd() / "tests/files" / name
+            with open(report_path, "w+") as report_file:
                 report_file.write(report)
 
-            filetype = mimetypes.guess_type(report_path / report_name)
-            report_stat = os.stat(report_path / report_name)
+            filetype = mimetypes.guess_type(report_path)
+            report_stat = os.stat(report_path)
             self.assertGreater(report_stat.st_size, 0, "Downloaded report is empty")
             self.assertIn(
                 filetype[0],
                 ("application/rdf+xml", "application/xml"),
                 "Downloaded report is not a RDF/XML file",
             )
-            Path(report_path / report_name).unlink()
-        except FossologyApiError as error:
-            logger.error(error.message)
-
-        try:
-            # Zip
-            report = foss.download_report(report_id, as_zip=True)
-            report_path = Path.cwd() / "tests/files"
-            report_name = upload_filename + ".spdx-report.rdf.zip"
-            with open(report_path / report_name, "w+") as report_file:
-                report_file.write(report)
-
-            filetype = mimetypes.guess_type(report_path / report_name)
-            report_stat = os.stat(report_path / report_name)
-            self.assertGreater(report_stat.st_size, 0, "Downloaded report is empty")
-            self.assertEqual(
-                filetype[0], "application/zip", "Downloaded report is not a ZIP file"
-            )
-            Path(report_path / report_name).unlink()
+            Path(report_path).unlink()
         except FossologyApiError as error:
             logger.error(error.message)
 
