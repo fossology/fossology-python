67da7a5d10824fb3901eadcf9a04b540aec426a3
Marion Deveaud <marion.deveaud@siemens.com>
2022-09-22 12:14:20 +0200

    feat(cli): add delete commands and use them in the tests

--- tests/test_foss_cli_create_cmds.py
+++ tests/test_foss_cli_create_cmds.py
@@ -1,5 +1,7 @@
 __doc__ = """Test 'create_*' sub commands of foss_cli"""
 
+import time
+
 from fossology import foss_cli
 
 
@@ -20,6 +22,19 @@ def test_create_folder(runner, click_test_dict):
     )
     assert result.exit_code == 0
     assert d["VERBOSE"] == 2
+    time.sleep(2)
+    # cleanup
+    result = runner.invoke(
+        foss_cli.cli,
+        [
+            "-vv",
+            "delete_folder",
+            "FossFolder",
+        ],
+        obj=d,
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
 
 
 def test_create_group(runner, click_test_dict):
