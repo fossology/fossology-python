a176a151a43b0a13a3199961b6ff33d51f843d64
Marion Deveaud <marion.deveaud@siemens.com>
2025-01-28 22:47:22 +0100

    chore: manually update version number in docs and pyproject.toml

--- docs-source/conf.py
+++ docs-source/conf.py
@@ -22,7 +22,7 @@ project = "fossology"
 copyright = "2021, Siemens AG"
 
 # The full version, including major/minor/patch tags
-release = "3.2.1"
+release = "3.3.0"
 
 
 # -- General configuration ---------------------------------------------------
